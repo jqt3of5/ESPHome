d4d480561d934e6b1a480028e8f07eeb594c08e3
jqt3of5 <jqt3of5@gmail.com>
2025-01-04 22:40:37 -0700

    Jusstkeep trying...

--- components/wen3410/__init__.py
+++ components/wen3410/__init__.py
@@ -2,9 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome import automation
-from esphome.const import CONF_ID, CONF_PIN, CONF_BUTTON
+from esphome.const import CONF_ID, CONF_PIN, CONF_BUTTON, CONF_VALUE
 from esphome.core import CORE
-from esphome.automation import maybe_simple_id
 from esphome.components import output
 
 wen3410ns = cg.esphome_ns.namespace("wen3410")
@@ -31,18 +30,23 @@ async def register_output(var, config):
         var = cg.Pvariable(config[CONF_ID], var)
     await setup_output_platform_(var, config)
 
-WEN3410_ACTION_SCHEMA = maybe_simple_id(
+WEN3410_ACTION_SCHEMA = cv.maybe_simple_value(
     {
         cv.Required(CONF_ID): cv.use_id(WEN3410Component),
-    }
+    },
+    key=CONF_VALUE
 )
 
 @automation.register_action(
     "wen3410.increase_delay", IncreaseDelayAction, WEN3410_ACTION_SCHEMA
 )
 async def wen3410_increase_delay_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    return cg.new_Pvariable(action_id, template_arg, paren)
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    template_ = await cg.templatable(config[CONF_VALUE], args, cg.uint16)
+    cg.add(var.set_value(template_))
+    return var
+
 
 @automation.register_action(
     "wen3410.increase_speed", IncreaseSpeedAction, WEN3410_ACTION_SCHEMA
